beb5351933ae6b32ee5de4ad906a91a25bb73a98
Jose Mario Gutierrez <jogutierrez@H7689.ad.hugeinc.com>
2018-10-14 19:51:32 -0500

    Created a wrapper for the flex-container and updated it's styles

--- src/FlexBox.js
+++ src/FlexBox.js
@@ -2,30 +2,43 @@ import React from 'react';
 import { Box } from './Box';
 import styled from 'styled-components';
 
-const Container = styled.div`
-  width: calc(100% - 300px);
-  height: 100vh;
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0 0 20px 20px;
+`;
+
+const FlexContainerText = styled.div`
+  font-size: 12px;
+  color: #5c339b;
+  margin-bottom: 2px;
+`
+
+const FlexContainer = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: ${({flexDirection}) => flexDirection};
   justify-content: ${({justifyContent}) => justifyContent};
   align-items: ${({alignItems}) => alignItems};
   align-content: ${({alignContent}) => alignContent};
   flex-wrap: ${({flexWrap}) => flexWrap};
-`;
+  padding: 2px;
+  border: 2px dashed #5c3799;
+`
 
-export const FlexBox = (props) => (
-  <Container {...props}>
-    <Box primary />
-    <Box />
-    <Box primary />
-    <Box />
-    <Box primary />
-    <Box />
-    <Box primary />
-    <Box />
-    <Box primary />
-    <Box />
-    <Box primary />
-    <Box />
-  </Container>
+export const FlexBoxWrapper = (props) => (
+  <Wrapper>
+    <FlexContainerText>flex-container</FlexContainerText>
+    <FlexContainer {...props}>
+      <Box>1</Box>
+      <Box>2</Box>
+      <Box>3</Box>
+      <Box>4</Box>
+      <Box>5</Box>
+      <Box>6</Box>
+      <Box>7</Box>
+      <Box>8</Box>
+    </FlexContainer>
+  </Wrapper>
 );
