295cfe868a72d312c558f4c836ec3a3b7a1151ad
Jose Mario Gutierrez <jogutierrez@H7689.ad.hugeinc.com>
2018-10-14 19:50:10 -0500

    Added Headline, and updated styles on the options container

--- src/FlexOptions.js
+++ src/FlexOptions.js
@@ -2,19 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 import { flexDirectionOptions, justifyContentOptions, alignItemsOptions, alignContentOptions, flexWrapOptions } from './options'
 import { Select } from './Select';
-import { Title } from './Title';
+
+const Headline = styled.div`
+  font-size: 20px;
+  text-align: center;
+  margin-bottom: 30px;
+  border-bottom: 1px solid #ededed;
+  padding-bottom: 15px;
+`;
 
 const Container = styled.div`
-  width: 300px;
-  height: 100vh;
-  padding: 10px;
-  border: 2px solid #333;
+  min-width: 250px;
+  height: 100%;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 2px;
   box-sizing: border-box;
+  box-shadow: 0 1px 5px 0 rgba(0,0,0,.1);
 `;
 
 export const FlexOptions = ({ onChange }) => (
   <Container>
-    <Title>display: flex</Title>
+    <Headline><strong>Flexbox</strong> Playground</Headline>
     <Select
       name="flexDirection"
       onChange={onChange}
