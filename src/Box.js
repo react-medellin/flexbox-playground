a3a27617c3d6df2e1894723552535d31409bae6a
Jose Mario Gutierrez <jogutierrez@H7689.ad.hugeinc.com>
2018-10-14 19:49:21 -0500

    Updated styles for box item, now it has 3 different colors and a number

--- src/Box.js
+++ src/Box.js
@@ -1,7 +1,20 @@
 import styled from 'styled-components';
 
 export const Box = styled.div`
-  background-color: ${props => props.primary ? '#5c3799' : '#a67d4c'};
+  background-color: #5c3799;
   height: 120px;
   width: 120px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 15px;
+  color: #fff;
+  margin: 2px;
+
+  &:nth-child(2n){
+    background-color: #7c65aa;
+  }
+  &:nth-child(3n){
+    background-color: #443377;
+  }
 `;
