96b544337968a2911664a20cc705b06343aae843
Jose Mario Gutierrez <jogutierrez@H7689.ad.hugeinc.com>
2018-10-14 19:48:03 -0500

    Modified Title component to have smaller size and lighter font-weight

--- src/Title.js
+++ src/Title.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const Title = styled.p`
-  color: '#333';
-  font-weight: 600;
-  margin-bottom: 10;
+  color: #3e3e3e;
+  font-size: 12px;
+  font-weight: lighter;
+  margin-bottom: 5px;
 `;
