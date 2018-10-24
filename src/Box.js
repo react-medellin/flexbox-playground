ec705f4921e77295477a8ffe022b76f0c7c32003
Sebastian Lacle <info@slacle.com>
2018-10-24 03:37:38 -0500

    add numbers to boxes

--- src/Box.js
+++ src/Box.js
@@ -1,7 +1,11 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Box = styled.div`
-  background-color: ${props => props.primary ? '#5c3799' : '#a67d4c'};
-  height: ${({height}) => height + 'px'};
-  width: ${({width}) => width + 'px'};
+  background-color: ${props => (props.primary ? "#5c3799" : "#a67d4c")};
+  height: ${({ height }) => height + "px"};
+  width: ${({ width }) => width + "px"};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
 `;
